53f86c33ce34ee3ba66914fb2a127b973a625d6a
Oleg <alligator2d@rambler.ru>
2023-01-13 11:35:49 +0300

    fourth commit

--- js/script.js
+++ js/script.js
@@ -1,71 +1,64 @@
-const input = document.querySelector("input");
-const btn = document.querySelector("button");
 
-document.querySelector('.main-Details').style.display = 'none';
-
-const t = Date.now();
-console.log(t);
-
-
-btn.addEventListener("click", async () => {
-	console.log(input.value);
-	const cityName = input.value;
-	const serverUrl = "http://api.openweathermap.org/data/2.5/";
-	const apiKey = "dd104ede60c26d64cf20852b80b4317d";
-	const url = `${serverUrl}weather?q=${cityName}&appid=${apiKey}`;
+window.addEventListener('DOMContentLoaded',function () {
+	let list = [];
 	
-	const data = await getData(url);
-	
-	console.log(data);
-	document.querySelector('.package-name').textContent = data.name;
-	document.querySelector(".price").innerHTML = Math.round(data.main.temp - 273) + "&deg;";
-	document.querySelector(".disclaimer").textContent = `Weather: ${data.weather[0]["description"]}`;
-	document.querySelector(".features li").innerHTML =
-		`<img src="https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png">`;
-	
-	
-	console.log(data.sys.sunrise);
-	console.log(data.sys.sunset);
-	const sunrise = JSON.parse(data.sys.sunrise);
-	console.log(sunrise);
-	console.log(getTime(sunrise));
-	
-	document.querySelector('.package-name-details').textContent = `City/town: ${data.name}`;
-	document.querySelector(".price-details").innerHTML = `Temperature: ${Math.round(data.main.temp - 273)} &deg;`;
-	document.querySelector('.price-feels').innerHTML = `Feels like: ${Math.round(data.main.feels_like - 273)} &deg;`
-	document.querySelector('.disclaimer-sunrise').textContent = `Sunrise: ${getTime(data.sys.sunrise)}`;
-	document.querySelector('.disclaimer-sunset').textContent = `Sunset: ${getTime(data.sys.sunset)}`;
-	
-	
-	input.value = "";
-	
-});
-
-const now = document.querySelector('.now');
-const details = document.querySelector('.details');
-
-now.addEventListener('click', () => {
-	
-	if(!now.classList.contains('active')) {
-		now.classList.add('active');
-		details.classList.remove('active');
-	} else {
-		return;
-	}
+	const input = document.querySelector("input");
+	const btn = document.querySelector("button");
 	document.querySelector('.main-Details').style.display = 'none';
-	document.querySelector('.main-Now').style.display = 'block';
+	
+	getFromLocalStorage(list);
+	
+	input.onkeydown = checkEnter;
+	
+	btn.addEventListener("click", async () => {
+		const cityName = input.value;
+		const serverUrl = "http://api.openweathermap.org/data/2.5/";
+		const apiKey = "dd104ede60c26d64cf20852b80b4317d";
+		const url = `${serverUrl}weather?q=${cityName}&appid=${apiKey}`;
+		const data = await getData(url);
+		createWeather(data);
+		
+	});
+	
+	const now = document.querySelector('.now');
+	const details = document.querySelector('.details');
+	
+	now.addEventListener('click', () => {
+		
+		if(!now.classList.contains('active')) {
+			now.classList.add('active');
+			details.classList.remove('active');
+		} else {
+			return;
+		}
+		document.querySelector('.main-Details').style.display = 'none';
+		document.querySelector('.main-Now').style.display = 'block';
+		
+	})
+	
+	details.addEventListener('click', () => {
+		
+		if(!details.classList.contains('active')) {
+			details.classList.add('active');
+			now.classList.remove('active');
+		} else {
+			return;
+		}
+		document.querySelector('.main-Now').style.display = 'none';
+		document.querySelector('.main-Details').style.display = 'block';
+		
+	})
+	
+	const like = document.querySelector('.like');
+	
+	like.addEventListener('click', () => {
+		createElem(input.value);
+		list.push(input.value);
+		setToLocalStorage(list);
+		input.value = '';
+		
+	})
+	
 	
 })
 
-details.addEventListener('click', () => {
-	
-	if(!details.classList.contains('active')) {
-		details.classList.add('active');
-		now.classList.remove('active');
-	} else {
-		return;
-	}
-	document.querySelector('.main-Now').style.display = 'none';
-	document.querySelector('.main-Details').style.display = 'block';
-	
-})
--- js/view/view.js
+++ js/view/view.js
@@ -7,6 +7,7 @@ async function getData(url) {
 		return await response.json();
 	} catch (error) {
 		console.log(error.message);
+		document.querySelector('input').value = 'Incorrect city name =(';
 	}
 }
 
@@ -14,11 +15,87 @@ function getTime(res) {
 	let hours = Math.floor(res / (1000 * 60 * 60) % 24);
 	let minutes = Math.floor((res / 1000 / 60) % 60);
 	
-	return `${hours}:${minutes}`
+	return `${hours}:${minutes}`;
 }
 
-let t1 = new Date(1673527617);
-let t2 = new Date(1673563824)
-console.log(`${t1.getHours()}:${t1.getMinutes()}`);
-console.log(`${t2.getHours()}:${t2.getMinutes()}`);
+// let t1 = new Date(1673527617);
+// let t2 = new Date(1673563824)
+// console.log(`${t1.getHours()}:${t1.getMinutes()}`);
+// console.log(`${t2.getHours()}:${t2.getMinutes()}`);
 
+function createElem(city17) {
+	const elem = document.createElement("div");
+	const deleteBtn = document.createElement("button");
+	
+	elem.className = "list";
+	elem.innerText = city17;
+	
+	deleteBtn.innerText = "x";
+	deleteBtn.classList.add("delete");
+	elem.appendChild(deleteBtn);
+	
+	document.querySelector(".add").append(elem);
+}
+
+
+
+function createWeather(data) {
+	console.log(data);
+	document.querySelector(".package-name").textContent = data.name;
+	document.querySelector(".price").innerHTML = Math.round(data.main.temp - 273) + "&deg;";
+	document.querySelector(".disclaimer").textContent = `Weather: ${data.weather[0]["description"]}`;
+	document.querySelector(".features li").innerHTML =
+		`<img src="https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png">`;
+	document.querySelector(".package-name-details").textContent = `City/town: ${data.name}`;
+	document.querySelector(".price-details").innerHTML = `Temperature: ${Math.round(data.main.temp - 273)} &deg;`;
+	document.querySelector(".price-feels").innerHTML = `Feels like: ${Math.round(data.main.feels_like - 273)} &deg;`;
+	document.querySelector(".disclaimer-sunrise").textContent = `Sunrise: ${getTime(data.sys.sunrise)}`;
+	document.querySelector(".disclaimer-sunset").textContent = `Sunset: ${getTime(data.sys.sunset)}`;
+	
+	// input.value = "";
+}
+
+async function checkEnter(e) {
+	const input = document.querySelector("input");
+	
+	if(e.key === "Enter" && input.value !== "") {
+		const cityName = input.value;
+		const serverUrl = "http://api.openweathermap.org/data/2.5/";
+		const apiKey = "dd104ede60c26d64cf20852b80b4317d";
+		const url = `${serverUrl}weather?q=${cityName}&appid=${apiKey}`;
+		const data = await getData(url);
+		createWeather(data);
+	}
+}
+
+function getFromLocalStorage() {
+	if(localStorage.getItem("list")) {
+		const localStorageItems = JSON.parse(localStorage.getItem("list"));
+		
+		createElem(localStorageItems);
+		// localStorageItems.forEach(i => createWeather(i));
+	}
+}
+
+function setToLocalStorage(city17) {
+	localStorage.setItem("list", JSON.stringify(city17));
+}
+
+const deleteBtn = document.querySelector('.delete');
+
+if(deleteBtn) {
+	deleteBtn.addEventListener("click", () => {
+		deleteElem();
+	});
+}
+
+function deleteElem() {
+	// const btn = e.target.closest(".delete");
+	// if(!btn) {
+	// 	return;
+	// }
+	// btn.parentElement.remove();
+	
+	console.log('delete');
+	localStorage.clear();
+}
